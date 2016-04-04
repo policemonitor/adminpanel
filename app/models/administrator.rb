a7e558a0f2f2d3241b89da77a75ecf7800da2148
Garmatniy <garmatniy314@gmail.com>
2016-04-05 00:47:51 +0300

    BEst design ever

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,7 +1,7 @@
 class Administrator < ActiveRecord::Base
   has_secure_password
 
-  validates :password,	presence: true, length: { minimum: 6 }, message: "Пароль повинен містити більше 6 символів"
-  validates :login,	presence: true, message: "Логін не може бути пустим"
-  validates :name,	presence: true, length: { minimum: 1 }, message: "Ім’я не може бути пустим"
+  validates :password, length: { minimum: 6 }, presence: { message: "Пароль повинен містити мінімум 6 символів" }
+  validates :login,	presence: true
+  validates :name,	presence: true, length: { minimum: 1 }
 end
