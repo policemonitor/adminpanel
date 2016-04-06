17c8b0ea787a4a5943afdfb338de6bb6123c7394
Roman Kaporin <rmkaporin@gmail.com>
2016-04-07 00:56:35 +0300

    Before merging with authorisation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,14 @@ class SessionsController < ApplicationController
     @administrator = Administrator.find_by_login(params[:session][:login])
     if @administrator && @administrator.authenticate(params[:session][:password])
       session[:administrator_id] = @administrator.id
-      redirect_to '/'
+      redirect_to root_path
     else
-      redirect_to 'login'
+      redirect_to login_path
     end
   end
 
   def destroy
     session[:administrator_id] = nil
-    redirect_to '/'
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   get 'signup' => 'administrators#new'
   resources :administrators
+  root :to => "administrators#new"
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
