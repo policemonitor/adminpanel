d18127f7a2b909b8fe6883464f8dc1096bfe0eb9
Roman Kaporin <rmkaporin@gmail.com>
2016-04-09 23:31:01 +0300

    Added access filter

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -1,5 +1,9 @@
 class AdministratorsController < ApplicationController
   include AdministratorsHelper
+  include SessionsHelper
+
+  before_action :signed_in_administrator, only: [:edit, :update, :show, :new]
+  before_action :correct_administrator,   only: [:edit, :update, :show, :new]
 
   def new
     @administrator = Administrator.new
@@ -25,9 +29,36 @@ class AdministratorsController < ApplicationController
     @administrator = current_administrator
   end
 
+  def edit
+    @administrator = current_administrator
+  end
+
+  def update
+    @administrator = Administrator.find(params[:id])
+    if @administrator.update_attributes(administrator_params)
+      flash[:success] = 'Обліковий запис змінено'
+      redirect_to @administrator
+    else
+      flash[:danger] = flash_errors(@administrator)
+      redirect_to current_administrator_edit_path
+    end
+  end
+
   private
 
   def administrator_params
     params.require(:administrator).permit(:lastname, :personal_number, :rank, :login, :password)
+  end
+
+  def signed_in_administrator
+    unless signed_in?
+      flash[:danger] = "Увійдіть до системи!"
+      redirect_to login_path
+    end
+  end
+
+  def correct_administrator
+    @administrator = current_administrator
+    redirect_to(root_path) unless current_administrator?(@administrator)
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  def current_administrator?(administrator)
+    administrator == current_administrator
+  end
+
+  def signed_in?
+   !current_administrator.nil?
+  end
 end
