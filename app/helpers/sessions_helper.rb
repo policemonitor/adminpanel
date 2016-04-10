f83a63854284af6e8e8bb6c7c7255bc094c7200b
Roman Kaporin <rmkaporin@gmail.com>
2016-04-10 13:34:12 +0300

    Edit Form completed

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,20 @@
 module SessionsHelper
+  ADMIN_RANK = 1
+  HR_RANK = 2
+
   def current_administrator?(administrator)
     administrator == current_administrator
   end
 
   def signed_in?
    !current_administrator.nil?
+  end
+
+  def signed_in_ADMIN?
+    current_administrator.rank == ADMIN_RANK
+  end
+
+  def signed_in_HR?
+    current_administrator.rank == HR_RANK
   end
 end
