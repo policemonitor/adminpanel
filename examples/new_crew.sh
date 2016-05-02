afe45dcf8458264b52a801efac10d9ff921e11b5
Roman Kaporin <rmkaporin@gmail.com>
2016-05-02 19:27:36 +0300

    Manual server synchronization complete

---
+++ examples/new_crew.sh
@@ -0,0 +1,1 @@
+curl  -v -X POST -d '{"car": {"car_number":"AA3322BB","vin_number":"JDL5JDNVBDLRJFK","crew_name":"2001"}}'  -H "Content-Type:application/json" -H "Accept:application/json" http://localhost:5000/cars
---
+++ lib/synchronization.rb
@@ -0,0 +1,60 @@
+module Synchronization
+  require 'json'
+  CARS_SERVER_ADDRESS = "http://localhost:5000/cars.json"
+
+  def add_crew_to_support_server(car)
+    uri = URI.parse(CARS_SERVER_ADDRESS)
+
+    post_params = {
+      :car_number => car.car_number,
+      :crew_name  => car.crew_name,
+      :vin_number => car.vin_number
+    }
+
+    req = Net::HTTP::Post.new(uri.path)
+    req.body = JSON.generate(post_params)
+    req["Content-Type"] = "application/json"
+    req["Accept"] = "application/json"
+
+    begin
+      http = Net::HTTP.new(uri.host, uri.port)
+      response = http.start {|htt| htt.request(req)}
+    rescue StandardError
+      logger.error "Unnable to POST information from cars server! (#{CARS_SERVER_ADDRESS})"
+      return false
+    end
+      logger.info "Synchronization is succesfull!"
+      return true
+  end
+
+  def daemon_synchronize
+    require 'open-uri'
+    begin
+      requests = JSON.load(open(CARS_SERVER_ADDRESS))
+
+    rescue StandardError
+      logger.error "Unnable to fetch information from cars server #{CARS_SERVER_ADDRESS} !"
+      logger.info  "Using old data about crews!"
+      return false
+    end
+
+    requests.each do |request|
+      logger.info  "Updating car with VIN #{request["details"]["vin_number"]}"
+      crew = Crew.find_by(
+                          crew_name: request["car"],
+                          vin_number: request["details"]["vin_number"],
+                          car_number: request["details"]["car_number"]
+                       )
+      if !crew.nil?
+        crew.update_attributes(
+                          longitude: request["details"]["longitude"],
+                          latitude: request["details"]["latitude"]
+                       )
+
+        logger.info "Crews #{request["car"]} coordinates succesfully updated!"
+      else
+        logger.error "There is no such car with VIN  #{request["details"]["vin"]}!"
+      end
+    end
+  end
+end
