a010ed511e3ea201513526064d8b983c384ccf38
Roman Kaporin <rmkaporin@gmail.com>
2016-05-29 18:45:02 +0300

    GPS allert added

--- app/helpers/crews_helper.rb
+++ app/helpers/crews_helper.rb
@@ -1,10 +1,14 @@
 module CrewsHelper
-  def flash_errors(crew)
+  def flash_errors crew
     notice = "Виникли помилки. Усього їх #{crew.errors.count}.<ul>"
     crew.errors.full_messages.each do |msg|
       notice += "<li>#{msg}</li>"
     end
     notice += "</ul>"
     return notice
+  end
+
+  def gps_unreachable crew
+    return (crew.latitude == nil || crew.longitude == nil) ? true : false
   end
 end
